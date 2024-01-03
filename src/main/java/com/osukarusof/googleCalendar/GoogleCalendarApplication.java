1b2744e390cb91bd78c54aec98592ddb0c7bd317
Oscar Sanchez <oscar.s.m0711@gmail.com>
2024-01-03 10:05:30 -0500

    chore: init proyect basic

---
+++ src/main/java/com/osukarusof/googleCalendar/GoogleCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.osukarusof.googleCalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCalendarApplication.class, args);
+	}
+
+}
